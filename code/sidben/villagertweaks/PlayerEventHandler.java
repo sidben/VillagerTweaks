6bbfded717f7293f106da94185ebd3ec34defd05
sidben <noemail>
2013-07-19 03:17:16 -0300

    Added feature #2

--- code/sidben/villagertweaks/PlayerEventHandler.java
+++ code/sidben/villagertweaks/PlayerEventHandler.java
@@ -1,5 +1,9 @@
 package sidben.villagertweaks;
 
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
 import cpw.mods.fml.relauncher.Side;
@@ -19,6 +23,28 @@ public class PlayerEventHandler {
 		 * OBS: This method is called whenever a player interacts with something (right-click)
 		 */
 
+		// check if the player right-clicked a villager
+        if (event.target instanceof EntityVillager)
+        {
+
+        	// Check if the player is holding a Name Tag
+            ItemStack item = event.entityPlayer.inventory.getCurrentItem();
+            if (item != null && item.itemID == Item.field_111212_ci.itemID)			// Name Tag
+            {
+
+            	if (item.hasDisplayName()) 
+            	{
+            		EntityLiving villager = (EntityLiving)event.target;
+            		villager.setCustomNameTag(item.getDisplayName());
+            		--item.stackSize;
+
+            		event.setCanceled(true);
+            	}
+
+            }
+
+            
+        }
 
 
        
